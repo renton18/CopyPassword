c964c82833a4c7a5dedad47ee8167e11f7b32660
renton18 <gakituka18@gmail.com>
2018-04-22 23:58:38 +0900

    タブレイアウト設定

--- CopyPassword/AddAccount.Designer.cs
+++ CopyPassword/AddAccount.Designer.cs
@@ -35,11 +35,13 @@
             this.label2 = new System.Windows.Forms.Label();
             this.name_tb = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // add_btn
             // 
-            this.add_btn.Location = new System.Drawing.Point(98, 87);
+            this.add_btn.Location = new System.Drawing.Point(126, 144);
             this.add_btn.Name = "add_btn";
             this.add_btn.Size = new System.Drawing.Size(106, 31);
             this.add_btn.TabIndex = 4;
@@ -49,16 +51,16 @@
             // 
             // id_tb
             // 
-            this.id_tb.Location = new System.Drawing.Point(57, 37);
+            this.id_tb.Location = new System.Drawing.Point(67, 37);
             this.id_tb.Name = "id_tb";
-            this.id_tb.Size = new System.Drawing.Size(147, 19);
+            this.id_tb.Size = new System.Drawing.Size(165, 19);
             this.id_tb.TabIndex = 2;
             // 
             // pass_tb
             // 
-            this.pass_tb.Location = new System.Drawing.Point(57, 62);
+            this.pass_tb.Location = new System.Drawing.Point(67, 62);
             this.pass_tb.Name = "pass_tb";
-            this.pass_tb.Size = new System.Drawing.Size(147, 19);
+            this.pass_tb.Size = new System.Drawing.Size(165, 19);
             this.pass_tb.TabIndex = 3;
             // 
             // label1
@@ -81,9 +83,9 @@
             // 
             // name_tb
             // 
-            this.name_tb.Location = new System.Drawing.Point(57, 12);
+            this.name_tb.Location = new System.Drawing.Point(67, 12);
             this.name_tb.Name = "name_tb";
-            this.name_tb.Size = new System.Drawing.Size(147, 19);
+            this.name_tb.Size = new System.Drawing.Size(165, 19);
             this.name_tb.TabIndex = 1;
             // 
             // label3
@@ -95,11 +97,30 @@
             this.label3.TabIndex = 6;
             this.label3.Text = "Name";
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "DETAIL";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(67, 87);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(165, 51);
+            this.textBox1.TabIndex = 7;
+            // 
             // AddAccount
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(214, 125);
+            this.ClientSize = new System.Drawing.Size(240, 179);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.name_tb);
             this.Controls.Add(this.label2);
@@ -123,5 +144,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox name_tb;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- CopyPassword/Form1.Designer.cs
+++ CopyPassword/Form1.Designer.cs
@@ -29,15 +29,17 @@
         private void InitializeComponent()
         {
             this.account_lb = new System.Windows.Forms.ListBox();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
             this.id_lb = new System.Windows.Forms.ListBox();
-            this.password_tb = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.Remark_tb = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.password_tb = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
             this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // account_lb
@@ -46,110 +48,137 @@
             this.account_lb.Dock = System.Windows.Forms.DockStyle.Fill;
             this.account_lb.FormattingEnabled = true;
             this.account_lb.ItemHeight = 12;
-            this.account_lb.Location = new System.Drawing.Point(0, 0);
+            this.account_lb.Location = new System.Drawing.Point(3, 3);
             this.account_lb.Name = "account_lb";
-            this.account_lb.Size = new System.Drawing.Size(146, 216);
+            this.account_lb.Size = new System.Drawing.Size(231, 134);
             this.account_lb.Sorted = true;
             this.account_lb.TabIndex = 0;
             this.account_lb.SelectedIndexChanged += new System.EventHandler(this.account_lb_SelectedIndexChanged);
-            // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.account_lb);
-            this.panel1.Location = new System.Drawing.Point(12, 49);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(146, 216);
-            this.panel1.TabIndex = 1;
-            // 
-            // panel2
-            // 
-            this.panel2.Controls.Add(this.id_lb);
-            this.panel2.Location = new System.Drawing.Point(164, 49);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(146, 216);
-            this.panel2.TabIndex = 2;
             // 
             // id_lb
             // 
             this.id_lb.Dock = System.Windows.Forms.DockStyle.Fill;
             this.id_lb.FormattingEnabled = true;
             this.id_lb.ItemHeight = 12;
-            this.id_lb.Location = new System.Drawing.Point(0, 0);
+            this.id_lb.Location = new System.Drawing.Point(240, 3);
             this.id_lb.Name = "id_lb";
-            this.id_lb.Size = new System.Drawing.Size(146, 216);
+            this.id_lb.Size = new System.Drawing.Size(231, 134);
             this.id_lb.Sorted = true;
             this.id_lb.TabIndex = 0;
             this.id_lb.SelectedIndexChanged += new System.EventHandler(this.id_lb_SelectedIndexChanged);
             // 
-            // password_tb
+            // Remark_tb
             // 
-            this.password_tb.Location = new System.Drawing.Point(71, 12);
-            this.password_tb.Name = "password_tb";
-            this.password_tb.PasswordChar = '*';
-            this.password_tb.Size = new System.Drawing.Size(239, 19);
-            this.password_tb.TabIndex = 4;
+            this.Remark_tb.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Remark_tb.Location = new System.Drawing.Point(477, 3);
+            this.Remark_tb.Multiline = true;
+            this.Remark_tb.Name = "Remark_tb";
+            this.Remark_tb.Size = new System.Drawing.Size(234, 134);
+            this.Remark_tb.TabIndex = 7;
             // 
-            // label1
+            // tableLayoutPanel1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 15);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(53, 12);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "password";
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Controls.Add(this.account_lb, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.Remark_tb, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.id_lb, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 53);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(714, 140);
+            this.tableLayoutPanel1.TabIndex = 8;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel1, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(720, 196);
+            this.tableLayoutPanel2.TabIndex = 9;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.ForeColor = System.Drawing.SystemColors.ActiveBorder;
-            this.label2.Location = new System.Drawing.Point(69, 34);
+            this.label2.Location = new System.Drawing.Point(70, 25);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(186, 12);
-            this.label2.TabIndex = 6;
+            this.label2.TabIndex = 12;
             this.label2.Text = "F1:追加　Ctrl + Shift:パスワード表示 ";
             // 
-            // Remark_tb
+            // label1
             // 
-            this.Remark_tb.Location = new System.Drawing.Point(316, 49);
-            this.Remark_tb.Multiline = true;
-            this.Remark_tb.Name = "Remark_tb";
-            this.Remark_tb.Size = new System.Drawing.Size(149, 213);
-            this.Remark_tb.TabIndex = 7;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "password";
+            // 
+            // password_tb
+            // 
+            this.password_tb.Location = new System.Drawing.Point(72, 3);
+            this.password_tb.Name = "password_tb";
+            this.password_tb.PasswordChar = '*';
+            this.password_tb.Size = new System.Drawing.Size(239, 19);
+            this.password_tb.TabIndex = 10;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.password_tb);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(714, 44);
+            this.panel1.TabIndex = 9;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(471, 277);
-            this.Controls.Add(this.Remark_tb);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.password_tb);
-            this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel1);
+            this.ClientSize = new System.Drawing.Size(720, 196);
+            this.Controls.Add(this.tableLayoutPanel2);
             this.KeyPreview = true;
             this.Name = "Form1";
             this.Text = "CopyPassword";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
-            this.panel2.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.ListBox account_lb;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.ListBox id_lb;
+        private System.Windows.Forms.TextBox Remark_tb;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox password_tb;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox Remark_tb;
     }
 }
 
